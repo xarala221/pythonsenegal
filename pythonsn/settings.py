028b37e41fc01cba3c12d1d6a55b7645510fedab
Ousseynou Diop <xaralaxarala@gmail.com>
2019-05-12 01:01:45 +0000

    add static config

--- pythonsn/settings.py
+++ pythonsn/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'pythonsn.urls'
 
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'pythonsn', 'templates')
 
 TEMPLATES = [
     {
@@ -121,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "pythonsn", "static"),
+]
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")

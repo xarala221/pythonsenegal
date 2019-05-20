a71f1ac81fdf2f98795fd2a71b5c18b5b03f4a07
Ousseynou Diop <xaralaxarala@gmail.com>
2019-05-20 16:28:21 +0000

    prepare to deploy

--- pythonsn/settings.py
+++ pythonsn/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "pythonsn", "static"),
+    os.path.join(BASE_DIR, "pythonsn/static"),
 ]
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
